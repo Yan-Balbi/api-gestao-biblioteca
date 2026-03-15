1306cf841f65d07dde80bdeca7cb16a5252a2206
Yan <yanbalbi@gmail.com>
2026-03-15 17:24:46 -0300

    feat: Implementando a estrutura básica do projeto.

---
+++ api-sistema-gestao-biblioteca/src/test/java/edu/yan/gestaobiblioteca/ApiSistemaGestaoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yan.gestaobiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaGestaoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
