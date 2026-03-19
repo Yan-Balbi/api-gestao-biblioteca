eb8b3f8e07cd98b22851456b921277fd97acfc89
Yan <yanbalbi@gmail.com>
2026-03-18 23:12:56 -0300

    Fix: adição do anotator '@RestController'

--- api-sistema-gestao-biblioteca/src/main/java/edu/yan/gestaobiblioteca/controller/UsuarioController.java
+++ api-sistema-gestao-biblioteca/src/main/java/edu/yan/gestaobiblioteca/controller/UsuarioController.java
@@ -7,11 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import edu.yan.gestaobiblioteca.model.UsuarioModel;
 import edu.yan.gestaobiblioteca.service.implementations.UsuarioImplementationService;
 
+@RestController
 public class UsuarioController {
 	@Autowired
 	private UsuarioImplementationService usuarioImplementationService;
