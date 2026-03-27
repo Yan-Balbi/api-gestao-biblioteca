f493caa4d98c6597feed599fd2e90b7ec3d671c2
Yan <yanbalbi@gmail.com>
2026-03-27 09:05:46 -0300

    feat: Implementação de model de suspensão.

---
+++ api-sistema-gestao-biblioteca/src/main/java/edu/yan/gestaobiblioteca/model/Suspensao.java
@@ -0,0 +1,35 @@
+package edu.yan.gestaobiblioteca.model;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Suspensao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable = false)
+	private Long id;
+	
+	//FetchType.LAZY precisa carregar os dados do banco
+	//FetchType.EAGER o objeto já vem com oss objetos associados carregados
+    @ManyToOne(fetch = FetchType.LAZY) // ou EAGER, dependendo do uso
+    @JoinColumn(name = "usuario_id", nullable = false) // define a coluna como FK
+    private UsuarioModel usuario;
+	
+	@CreationTimestamp
+    @Column(updatable = false, name = "data_inicio")
+	private Date dataInicio;
+	
+	@Column(nullable = true,name = "data_fim")
+	private Date dataFim;
+}
