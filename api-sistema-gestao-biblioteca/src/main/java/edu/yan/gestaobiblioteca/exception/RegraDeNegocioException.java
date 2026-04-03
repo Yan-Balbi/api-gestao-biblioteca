e0bc72676f3e880c46ed5c830501ceeb5a763c75
Yan <yanbalbi@gmail.com>
2026-04-03 16:54:14 -0300

    chore: Removendo exception inutil.

--- api-sistema-gestao-biblioteca/src/main/java/edu/yan/gestaobiblioteca/exception/RegraDeNegocioException.java
+++
@@ -1,7 +0,0 @@
-package edu.yan.gestaobiblioteca.exception;
-
-public class RegraDeNegocioException extends RuntimeException {
-	public RegraDeNegocioException(String mensagem) {
-		super (mensagem);
-	}
-}
